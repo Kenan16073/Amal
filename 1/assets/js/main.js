1dfe5ba24d575fa7db6fd9c65633b5087356586f
Kenan <kananmh@code.edu.az>
2024-06-24 00:44:38 +0400

    js

--- 1/assets/js/main.js
+++ 1/assets/js/main.js
@@ -200,26 +200,3 @@ document.addEventListener("DOMContentLoaded", () => {
   counter("count2", 100, 50, 2500);
   counter("count3", 0, 80, 3000);
 });
-
-
-
-// // Get the modal
-// let modal = document.getElementById("myModal");
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// let img = document.getElementById("myImg");
-// let modalImg = document.getElementById("img01");
-// let captionText = document.getElementById("caption");
-// img.onclick = function () {
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// let span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// }
